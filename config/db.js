ce5d4ae9b37d558bcdeddaf1c4a34fc258adb78a
Miguel <miguelbermell@gmail.com>
2021-05-23 12:42:23 +0200

    Ejercicios Resueltos express-crud Miguel

--- config/db.js
+++ config/db.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require("sequelize");
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-
 const dbConnection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "mysql",
+  dialectOptions: {
+    encrypt: false,
+    options: {
+      useUTC: false, // for reading from database
+    },
+  },
 });
 
 dbConnection.sync().then(() => console.log("DB Loaded"));
